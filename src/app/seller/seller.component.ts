cea16a03eb2d62b4ed419f9f4d24c665f175d201
Rohitchopade9822 <rohitprofessional99@gmail.com>
2024-10-03 18:12:38 +0530

    n

--- src/app/seller/seller.component.ts
+++ src/app/seller/seller.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SellService } from '../services/sell.service';
+import { SellerHomeComponent } from '../seller-home/seller-home.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-seller',
@@ -6,7 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller.component.css']
 })
 export class SellerComponent {
+
+  constructor(private sell:SellService, private router:Router){}
+  ngonInit():void{}
   SignUp(data:object):void{
-    console.warn(data)
+  
+    this.sell.userSignUp(data).subscribe((result)=>{
+
+    if(result)
+    {
+      
+      this.router.navigate(['seller-home'])
+    }
+    });
+
   }
 }
