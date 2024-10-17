157a472f184fd60c2d28a551ba5337fa9aa5911c
Rohitchopade9822 <rohitprofessional99@gmail.com>
2024-10-17 12:29:22 +0530

    home page desinge

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   
 })
 export class HomeComponent {
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images = [944, 1011, 884].map((n) => `https://picsum.photos/id/${n}/1400/600`);
 }
